727627ff94be9f78713062aed6aa279580a4322d
Bel68899 <112783504+Bel68899@users.noreply.github.com>
2023-05-16 11:51:30 -0300

    Create agenda.js

---
+++ agenda.js
@@ -0,0 +1,9 @@
+function Cadastro(){
+
+    var nome= document.getElementById("nome").value;
+    var siape= document.getElementById("siape").value;
+    var email= document.getElementById("email").value;
+    var area= document.getElementById("area").value;
+    var horas= document.getElementById("horas").value;
+    
+}
